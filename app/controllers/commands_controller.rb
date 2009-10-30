56033d86709cf738c62ff0bb5662b3997afa4370
Caue Guerra <caueguerra@gmail.com>
2009-10-30 19:21:36 -0200

    only show the available actions

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -3,10 +3,22 @@ class CommandsController < ApplicationController
     
   end
   
+  def show_rels
+    @from_url = params['url']
+    xml = Net::HTTP.get(URI.parse(params['url']))
+    order = Order.from_xml(xml)
+    @possible_states = order._possible_states.keys
+  end
+  
   def execute
     @from_url = params['url']
     xml = Net::HTTP.get(URI.parse(params['url']))
     order = Order.from_xml(xml)
     @response = order.send(params["command"])
+    
+    xml = Net::HTTP.get(URI.parse(params['url']))
+    order = Order.from_xml(xml)
+    
+    @possible_states = order._possible_states.keys
   end
 end
