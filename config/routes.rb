ad75c43de641ce7bf4463999b28ae13a0e3629ad
guilherme silveira <guilherme.silveira@caelum.com.br>
2009-10-30 14:54:14 -0200

    welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
+
+  map.root :controller => :welcome, :action => :welcome
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
