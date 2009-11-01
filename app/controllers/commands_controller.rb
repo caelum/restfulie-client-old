82e85e19236f1ee06c03781ddacf9916a8d439da
guilherme silveira <guilherme.silveira@caelum.com.br>
2009-11-01 18:20:01 -0200

    using new version from_web

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -5,20 +5,22 @@ class CommandsController < ApplicationController
   
   def show_rels
     @from_url = params['url']
+    # example on how to use it via xml content
     @xml = Net::HTTP.get(URI.parse(params['url']))
     order = Order.from_xml(@xml)
     @possible_states = order._possible_states.keys
   end
   
   def execute
+    
     @from_url = params['url']
-    xml = Net::HTTP.get(URI.parse(params['url']))
-    order = Order.from_xml(xml)
+    # example on how to read it straight from the web!!!
+    order = Order.from_web @from_url
     @response = order.send(params["command"])
-    
-    xml = Net::HTTP.get(URI.parse(params['url']))
-    order = Order.from_xml(xml)
-    
+
+    # refreshes it
+    order = Order.from_web @from_url
     @possible_states = order._possible_states.keys
+    
   end
 end
